2aec1e9a2c245aac50287ca0340f9dca570d979d
vigni1998 <53808836+vigni1998@users.noreply.github.com>
2019-08-06 13:06:38 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,8 +1,8 @@
 a=int(input()):
-if a>0:
-if(a%2==0):
-print("even")
+if a > 0:
+  if(a%2==0):
+    print("even")
+   else:
+    print("Odd")
 else:
-print("Odd")
-else:
-print("invalid")
+   print("invalid")
